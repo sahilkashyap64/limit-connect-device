56fbf2c4244434755573b236d01f48dda5ab2075
Sahil Kashyap <32007662+sahilkashyap64@users.noreply.github.com>
2020-04-16 20:11:12 +0530

    device are decrementing on each logout properly

--- Helper.php
+++ Helper.php
@@ -3,7 +3,7 @@ use Carbon\Carbon;
 use Jenssegers\Agent\Agent;
 /// check for 3 concurrent login
      function checkLoginLimit($user_id,$token){
-        $login_limit = 3;
+        $login_limit = 3; $device_limit = 3;
     //find all by user_id and and status=active
     $active_session = App\SessionTable::where([
     ['status', '=', '1'],
@@ -26,10 +26,10 @@ use Jenssegers\Agent\Agent;
         $task->user_ip = getUserIpAddr();
         $task->mac_address = 'from login entry';
         $task->connected_devices = 1;
-       $task->logintime = $date;;
+       $task->logintime = Carbon::now();
         $task->status = 1;
         $task->created_on = $date;
-        $task->modified_on = $date;
+        $task->modified_on = Carbon::now();
          
         if($task->save()){
         return response()->json([
@@ -42,8 +42,7 @@ use Jenssegers\Agent\Agent;
           
       ])->send();}
 
-        }
-        else if (count($active_session) > 0 && count($active_session) < $login_limit) {
+        } else if (count($active_session) > 0 && count($active_session) < $login_limit) {
 
          //new entry of sessionTable
          //and get the number of last connected device
@@ -51,13 +50,15 @@ use Jenssegers\Agent\Agent;
           ->where([
           ['status', '=', '1'],
           ['users_id', '=', $user_id],
-           ])->orderBy('id', 'desc')
+           ])->orderBy('connected_devices', 'desc')
            ->first();
            
          //echo $prevConnectDevicecount['connected_devices']; 
         $device=number_format($prevConnectDevicecount['connected_devices']); 
         //the number of device previously connected + 1
+        
         $newnumofDevice =$device+1; 
+        
         $date = date('Y-m-d H:i:s');
         
         //Store the login info in 
@@ -68,10 +69,10 @@ use Jenssegers\Agent\Agent;
         $task->user_ip = getUserIpAddr();
         $task->mac_address = 'from login entry with +1 in connected device';
         $task->connected_devices= $newnumofDevice;
-        $task->logintime = $date;;
+        $task->logintime = Carbon::now();
         $task->status = 1;
         $task->created_on = $date;
-        $task->modified_on = $date;
+        $task->modified_on = Carbon::now();
         $task->save();
         if($task->save()){
           
@@ -86,83 +87,6 @@ use Jenssegers\Agent\Agent;
         
        
 
-    }else if (count(($active_session)) >= $login_limit) {
-        //automatic logout
-        // $prevConnectLoginIntime = App\SessionTable::select('logintime')->addSelect('id')
-        //   ->where([
-        //   ['status', '=', '1'],
-        //   ['users_id', '=', $user_id],
-        //    ])->orderBy('modified_on', 'desc')
-        //    ->get()->toArray();
-         //  print_r($prevConnectLoginIntime); 
-          //get last login entry of the 
-          
-           $data = App\SessionTable::select('logintime')->addSelect('id')
-           ->where([
-           ['status', '=', '1'],
-           ['users_id', '=', $user_id],
-            ])->orderBy('modified_on', 'desc')
-            ->get();
-            $myarr=[];
-            foreach($data as $fieldnam) {
-              //increase this time when creating cms for now time is 2 minutes
-              $logintime=$fieldnam->logintime;
-              $dateTimeMinutesAgo = new DateTime("2 minutes ago");
-              $dateTimeMinutesAgo = $dateTimeMinutesAgo->format("Y-m-d H:i:s");
-            //   echo "\r\n Login:";
-            //   echo $logintime;
-            //   echo "\r\n datetimeTwominutes ago:";
-            //   echo $dateTimeMinutesAgo;
-              if ($logintime <  $dateTimeMinutesAgo){
-              //  echo "true";
-              
-               array_push($myarr,$fieldnam->id );                
-        
-          }
-          
-           }
-           //echo 'ids';
-          // print_r($myarr);
-           //'connected_devices'=> DB::raw('connected_devices-1'),
-           $decremen=count($myarr);// number of id we will use this number to decrement the last value
-           $updatedExpireConcrenteUser=App\SessionTable::whereIn('id', $myarr)->update(['status' => 0,'modified_on' => Carbon::now()]);
-         //  $updatedConnectedDeviceNum=App\SessionTable::whereIn('id', $myarr)->update(['connected_devices' => 0,'modified_on' => Carbon::now()]);
-           
-           if(!is_null($myarr)){
-
-        //check max device connected with user_id
-        
-           $prevConnectDeviceMaxcount=App\SessionTable::where([
-            ['status', '=', 1],
-            ['users_id', '=',$user_id ]
-        ])->orderBy('connected_devices','desc')->first(); //echo 'Max count';
-           $maxdevice=$prevConnectDeviceMaxcount['connected_devices']; 
-          $lastid=$prevConnectDeviceMaxcount['id'];
-           
-           
-           
-           $newnumofDevice= $maxdevice-$decremen;  //newNumofDevice
-           if(!$lastid== ''){
-           
-               //now update the new number of device in last active id
-               App\SessionTable::where('id', $lastid)->update([
-                'connected_devices' => $newnumofDevice
-                ]);
-            }
-             }
-           
-           
-           if($updatedExpireConcrenteUser) {
-             
-            return response()->json([
-                'success' => true,
-                'msg'=>'logged out automatically successful',
-                'myid'=>$myarr,
-            ])->send();
-        } 
-         
-           
-         
     } //after iteration we check if the count of logins is still greater than the limit
     if (count($active_session) >= $login_limit) {
         //then return a login error that maximum logins reached
@@ -194,6 +118,126 @@ use Jenssegers\Agent\Agent;
 
     
 }
+function automaticLogoutWithInterval($user_id){
+  $login_limit = 3;
+  //find all by user_id and and status=active
+  $active_session = App\SessionTable::where([
+  ['status', '=', '1'],
+  ['users_id', '=', $user_id],
+   ])->get()->toArray();
+if (count(($active_session)) >= $login_limit) {
+  //automatic logout
+  // $prevConnectLoginIntime = App\SessionTable::select('logintime')->addSelect('id')
+  //   ->where([
+  //   ['status', '=', '1'],
+  //   ['users_id', '=', $user_id],
+  //    ])->orderBy('modified_on', 'desc')
+  //    ->get()->toArray();
+   //  print_r($prevConnectLoginIntime); 
+    //get last login entry of the 
+    
+     $data = App\SessionTable::select('logintime')->addSelect('id')
+     ->where([
+     ['status', '=', '1'],
+     ['users_id', '=', $user_id],
+      ])->orderBy('modified_on', 'desc')
+      ->get();
+      $myarr=[];
+      foreach($data as $fieldnam) {
+        //increase this time when creating cms for now time is 2 minutes
+        $logintime=$fieldnam->logintime;
+        $dateTimeMinutesAgo = new DateTime("2 minutes ago");
+        $dateTimeMinutesAgo = $dateTimeMinutesAgo->format("Y-m-d H:i:s");
+      //   echo "\r\n Login:";
+      //   echo $logintime;
+      //   echo "\r\n datetimeTwominutes ago:";
+      //   echo $dateTimeMinutesAgo;
+        if ($logintime <  $dateTimeMinutesAgo){
+        //  echo "true";
+        
+         array_push($myarr,$fieldnam->id );                
+  
+    }
+    
+     }
+     //echo 'ids';
+    // print_r($myarr);
+     //'connected_devices'=> DB::raw('connected_devices-1'),
+     $decremen=count($myarr);// number of id we will use this number to decrement the last value
+     $updatedExpireConcrenteUser=App\SessionTable::whereIn('id', $myarr)->update(['status' => 0,'modified_on' => Carbon::now()]);
+   //  $updatedConnectedDeviceNum=App\SessionTable::whereIn('id', $myarr)->update(['connected_devices' => 0,'modified_on' => Carbon::now()]);
+     
+     if(!is_null($myarr)){
+
+  //check max device connected with user_id
+  
+     $prevConnectDeviceMaxcount=App\SessionTable::where([
+      ['status', '=', 1],
+      ['users_id', '=',$user_id ]
+  ])->orderBy('connected_devices','desc')->first(); //echo 'Max count';
+     $maxdevice=$prevConnectDeviceMaxcount['connected_devices']; 
+    $lastid=$prevConnectDeviceMaxcount['id'];
+     
+     
+     
+     $newnumofDevice= $maxdevice-$decremen;  //newNumofDevice
+     if(!$lastid== ''){
+     
+         //now update the new number of device in last active id
+         App\SessionTable::where('id', $lastid)->update([
+          'connected_devices' => $newnumofDevice
+          ]);
+      }
+       }
+     
+     
+     if($updatedExpireConcrenteUser) {
+       
+      return response()->json([
+          'success' => true,
+          'msg'=>'logged out automatically successful',
+          'myid'=>$myarr,
+      ])->send();
+  } 
+   
+     
+ //automatic logout finish  
+}}
+
+function LogoutandDecrementDevice($user_id){
+  $login_limit = 3;
+  //find all by user_id and and status=active
+  $active_session = App\SessionTable::where([
+  ['status', '=', '1'],
+  ['users_id', '=', $user_id],
+   ])->get()->toArray();
+if (count(($active_session)) <= $login_limit) {
+  //automatic logout
+  $myarr=[];
+  array_push($myarr,$user_id ); 
+     
+     $decremen=count($myarr);// number of id we will use this number to decrement the last value
+     $updatedExpireConcrenteUser=App\SessionTable::whereIn('id', $myarr)->update(['status' => 0,'modified_on' => Carbon::now()]);
+     
+     
+ //if logout happens minus one from all the active device of user
+ //UPDATE `session_table` SET `connected_devices` = `connected_devices` - 1 WHERE `status` = 1 AND `connected_devices`<> 1
+    App\SessionTable::where([
+            ['status', '=', '1'],
+            ['connected_devices', '!=', '1'],
+             ])
+    ->update([
+        'connected_devices' => DB::raw('`connected_devices` - 1 ')
+    ]);
+    
+      return response()->json([
+        'success' => true,
+        'msg'=>'logged out  successful',
+        'myid'=>$myarr,
+    ])->send();
+    
+     
+}}
 
 function getUserIpAddr(){
   if(!empty($_SERVER['HTTP_CLIENT_IP'])){
